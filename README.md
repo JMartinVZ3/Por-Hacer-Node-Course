8bec777666aedcfaec712e16e57dbd702ab2d0fc
Martin <jmartinvz@outlook.com>
2020-10-25 13:16:16 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Aplicacion de comandos
+
+este es uno de los ejercicios del curso
+
+recuerden instalar los paquetes de node
+
+npm install
---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+const descripcion = {
+    demmand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Elimina una tarea', {
+        descripcion
+    })
+    .command('listar', 'Listar toda la informacion')
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
